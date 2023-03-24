da1ec429f2dfd3c0437e8bafc34ef6dfc4fa4c53
Gabriele Bellicini <gabriele.bellicini@gmail.com>
2023-03-24 12:32:52 +0100

    Added section to compile the source code in a shared library object.

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Create the shared library object
+gcc -c -fPIC GOST.c -o GOST.o
+gcc -shared GOST.o -o GOST.so
+
+# Compile again the source into an executable, just to test the program
 gcc GOST.c -o GOST.o
 ./GOST.o 10 2
